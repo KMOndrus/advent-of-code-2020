cf7eaae71650098f274ed867001f6b0056e04bc1
KMOndrus <kloring@gmail.com>
2020-12-05 09:11:00 -0600

    Initial check-in

---
+++ index.js
@@ -0,0 +1,5 @@
+/*****************************************************************
+ * This project compiles all work to solve 2020 Advent of Code
+ * puzzles.  Each puzzle is in it's own file and can be run by
+ * uncommenting the function call below and using npm run start
+ ****************************************************************/
