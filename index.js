f229c181bdbd4978c73a1a51e010b71c2d6a6b3f
KMOndrus <kloring@gmail.com>
2020-12-10 22:31:53 -0600

    Day 2 Puzzle 2 completed

--- index.js
+++ index.js
@@ -7,7 +7,9 @@
 const { day1Puzzle1 } = require('./puzzles/day1Puzzle1/day1Puzzle1');
 const { day1Puzzle2 } = require('./puzzles/day1Puzzle2/day1Puzzle2');
 const { day2Puzzle1 } = require('./puzzles/day2Puzzle1/day2Puzzle1');
+const { day2Puzzle2 } = require('./puzzles/day2Puzzle2/day2Puzzle2');
 
 // day1Puzzle1();
 // day1Puzzle2();
-day2Puzzle1();
+// day2Puzzle1();
+day2Puzzle2();
---
+++ puzzles/day2Puzzle2/day2Puzzle2.js
@@ -0,0 +1,44 @@
+'use strict';
+const { readInterface } = require('../../utils/fileReader');
+
+const day2Puzzle2 = async () => {
+  const readFile = readInterface('puzzles/day2Puzzle1/puzzleInput.txt');
+  let numberOfValidPasswords = 0;
+
+  for await (const line of readFile){
+    const lineParts = line.split(' ');
+    const characterLocations = getCharacterLocations(lineParts[0]);
+    const requiredCharacter = getRequiredCharacter(lineParts[1]);
+    const charsAtSpecifiedLocations = getCharsAtSpecifiedLocations(lineParts[2], characterLocations);
+    if (isPasswordValid(requiredCharacter, charsAtSpecifiedLocations)){
+      ++numberOfValidPasswords;
+    }
+  }
+  console.log(numberOfValidPasswords)
+};
+
+const getCharacterLocations = inputString => {
+  const characterRange = inputString.split('-');
+  return characterRange.map(num => parseInt(num));
+};
+
+const getRequiredCharacter = inputString => {
+  const colonIndex = inputString.indexOf(":");
+  return inputString.substring(0, colonIndex);
+};
+
+const getCharsAtSpecifiedLocations = (string, stringLocationsArray) => {
+  const char1 = string.charAt(stringLocationsArray[0] - 1);
+  const char2 = string.charAt(stringLocationsArray[1] - 1);
+  return [char1, char2];
+};
+
+const isPasswordValid = (requiredChar, locationCharArray) => {
+  if (locationCharArray[0] === locationCharArray[1]){
+    return false;
+  } else return locationCharArray.includes(requiredChar);
+};
+
+module.exports = {
+  day2Puzzle2
+};
