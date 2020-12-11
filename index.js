604fec0e3cad1b5dc3952ea9622cd07fbf808795
KMOndrus <kloring@gmail.com>
2020-12-10 23:49:42 -0600

    Day 3 Puzzle 1 completed

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ const { day1Puzzle1 } = require('./puzzles/day1Puzzle1/day1Puzzle1');
 const { day1Puzzle2 } = require('./puzzles/day1Puzzle2/day1Puzzle2');
 const { day2Puzzle1 } = require('./puzzles/day2Puzzle1/day2Puzzle1');
 const { day2Puzzle2 } = require('./puzzles/day2Puzzle2/day2Puzzle2');
+const { day3Puzzle1 } = require('./puzzles/day3Puzzle1/day3Puzzle1');
 
 // day1Puzzle1();
 // day1Puzzle2();
+
 // day2Puzzle1();
-day2Puzzle2();
+// day2Puzzle2();
+
+day3Puzzle1();
---
+++ puzzles/day3Puzzle1/day3Puzzle1.js
@@ -0,0 +1,25 @@
+'use strict';
+const { readInterface } = require('../../utils/fileReader');
+
+const day3Puzzle1 = async () => {
+  const readFile = readInterface('puzzles/day3Puzzle1/puzzleInput.txt');
+  let lineCount = 0;
+  let treesIWillHit = 0;
+
+  for await (const line of readFile) {
+    const indexInLine = lineCount * 3;
+    let newLine = line;
+    while (indexInLine >= newLine.length) {
+      newLine += line;
+    }
+    if (newLine.charAt(indexInLine) === '#') {
+      ++treesIWillHit;
+    }
+    ++lineCount;
+  }
+  console.log(treesIWillHit);
+};
+
+module.exports = {
+  day3Puzzle1
+};
